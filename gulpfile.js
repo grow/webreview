bcff4586a5ce3bca273822924ba8d717c2854cff
jeremydw <jeremydw@gmail.com>
2014-07-10 11:43:10 -0700

    Add bower and gulp configs for frontend dependency management.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+
+
+var Path = {
+  CSS_OUT_DIR: './dist/css/',
+  CSS_SOURCES: './jetway/frontend/static/css/*.css',
+  JS_OUT_DIR: './dist/js/',
+  JS_SOURCES: './jetway/frontend/static/js/*.js',
+};
+
+
+gulp.task('minify', function(){
+  var css_sources = [
+    './bower_components/bootstrap/dist/css/bootstrap.min.css',
+    Path.CSS_SOURCES,
+  ];
+  gulp.src(css_sources)
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest(Path.CSS_OUT_DIR));
+
+  gulp.src(Path.JS_SOURCES)
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(Path.JS_OUT_DIR))
+    .pipe(uglify())
+    .pipe(gulp.dest(Path.JS_OUT_DIR));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(Path.JS_SOURCES, ['minify']);
+});
+
+
+gulp.task('default', ['minify', 'watch']);
